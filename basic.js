bf25dc7bf3123187ed208f4e0e28e405d5870f88
Ellen B Headrick <EBHeadrick@hotmail.com>
2014-06-17 19:35:16 -0600

    Our examples in class for today 17 Jun 2014

---
+++ basic.js
@@ -0,0 +1,24 @@
+
+console.log("Hello Ellen, how are you?");
+
+["Hi EB","Have a good day!","Any amount of vars in the array"].forEach(function term(x){console.log(x)});
+
+var y=["Hi EB","Have a good day!","Any amount of vars in the array"];
+console.log(y);
+
+var me = {
+    name: "Ellen B Headrick",
+    nickname: "EB",
+    age: "too old"
+};
+var i;
+
+for (i in me)console.log(me[i]);
+
+//me (function term(x) {console.log(x)});
+/*
+Comments :-)
+
+syntax
+var x=value  ,  y  , ... ;
+ */
